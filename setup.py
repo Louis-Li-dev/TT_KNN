feaba85ced43386964c4aa0c5f2f2bf713c1f9ea
AN SYU LI <yessir0621@gmail.com>
2025-01-05 13:04:03 +0800

    fix: setup issues

--- setup.py
+++ setup.py
@@ -3,22 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name='TT_KNN',
     version='1.0.0',
-    packages=find_packages(),
+    packages=find_packages(include=["ttknn", 'utility', 'utility.*']),
     author="AnSyu Li",
     author_email="yessir0621@gmail.com",  
+    license="MIT",
     description="A package for temporal trajectory analysis using K-Nearest Neighbors",
     url="https://github.com/Louis-Li-dev/TT_KNN",  
+    include_package_data=True,
     install_requires=[
         "pandas",
         "tqdm",
         "scikit-learn",
         "matplotlib",
         "numpy",
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",  
-        "Operating System :: OS Independent",
     ],
 
 )
