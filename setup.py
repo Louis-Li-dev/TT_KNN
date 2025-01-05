590af836ff2a8caa80465c1b6f582e757b91107d
AN SYU LI <yessir0621@gmail.com>
2025-01-05 13:22:11 +0800

    feat: setup

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     author_email="yessir0621@gmail.com",
     license="MIT",
     description="A package for temporal trajectory analysis using K-Nearest Neighbors",
-    long_description=open("README.md").read(),  # Use README.md for detailed description
     long_description_content_type="text/markdown",  # Specify Markdown format
     url="https://github.com/Louis-Li-dev/TT_KNN",
     include_package_data=True,  # Includes non-Python files specified in MANIFEST.in
