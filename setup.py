c0c50d2893a7527603fc491713c8ee7fbf08ea13
AN SYU LI <yessir0621@gmail.com>
2025-01-05 12:56:01 +0800

    fix: setup issues

--- setup.py
+++ setup.py
@@ -5,12 +5,22 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     author="AnSyu Li",
+    author_email="yessir0621@gmail.com",  
+    description="A package for temporal trajectory analysis using K-Nearest Neighbors",
+    long_description=open("README.md").read(), 
+    long_description_content_type="text/markdown", 
+    url="https://github.com/",  
     install_requires=[
-        "pandas>=2.0.3",
+        "pandas",
         "tqdm",
         "scikit-learn",
         "matplotlib",
         "numpy",
     ],
-)
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Replace with your license
+        "Operating System :: OS Independent",
+    ],
 
+)
