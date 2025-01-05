5d796507339991858fa90446e06b3078175fd4c3
AN SYU LI <yessir0621@gmail.com>
2025-01-05 13:10:52 +0800

    fix: setups

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='TT_KNN',
     version='1.0.0',
-    packages='ttknn',
+    packages=['ttknn'],
     author="AnSyu Li",
     author_email="yessir0621@gmail.com",  
     license="MIT",
