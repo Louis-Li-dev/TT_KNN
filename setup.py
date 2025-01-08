fe78ea358f2e440cddcd3ca7884e1ebbc14f3c93
AN SYU LI <yessir0621@gmail.com>
2025-01-08 21:59:27 +0800

    docs: version fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='TT_KNN',
-    version='1.1.2', 
+    version='1.1.3', 
     packages=find_packages(include=['ttknn', 'ttknn.*']),  
     author="AnSyu Li",
     author_email="yessir0621@gmail.com",
